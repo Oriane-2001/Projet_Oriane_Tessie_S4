302efa193924e18a5a086f39f32564659888f0a7
Neith12 <81319608+Neith12@users.noreply.github.com>
2021-03-26 18:21:01 +0100

    Update details_recette.sql

--- details_recette.sql
+++ details_recette.sql
@@ -4,3 +4,57 @@ INSERT INTO `CATEGORIES` ( `ID_CATEGORIE`, `NOM_CATEGORIE` )
 			( '2', 'Moins de 5€' ),
 			( '3', 'Moins de 10€' );
 
+INSERT INTO `INGREDIENTS` ( `ID_INGREDIENT`, `NOM_INGREDIENT` )
+		VALUES
+			( '1', 'Moins de 2€' ),
+			( '2', 'Moins de 5€' ),
+			( '3', 'Moins de 10€' );
+
+INSERT INTO `INTERNAUTES` (`ID_INTERNAUTE`,`NOM_INTERNAUTE` , `PRENOM_INTERNAUTE`, `AGE_INTERNAUTE`, `MAIL_INTERNAUTE`, `MDP_INTERNAUTE`,`PSEUDO_INTERNAUTE` )
+		VALUES
+			( '1', 'Moins de 2€' ),
+			( '2', 'Moins de 5€' ),
+			( '3', 'Moins de 10€' );
+			
+INSERT INTO `ETAPES` (`ID_ETAPE` , `NUMERO_ETAPE`,`CONTENU_ETAPE` )
+		VALUES
+			( '1', 'Moins de 2€' ),
+			( '2', 'Moins de 5€' ),
+			( '3', 'Moins de 10€' );
+			
+INSERT INTO `RECETTES` ( `ID_RECETTE`, `NOM_RECETTE`, `DATE_PUBLICATION_RECETTE` , `ID_CATEGORIE`, `NB_PERSONNE` )
+		VALUES
+			( '1', 'Moins de 2€' ),
+			( '2', 'Moins de 5€' ),
+			( '3', 'Moins de 10€' );
+			
+			
+INSERT INTO `CHOIX` ( `ID_RECETTE, `ID_INTERNAUTE` )
+		VALUES
+			( '1', 'Moins de 2€' ),
+			( '2', 'Moins de 5€' ),
+			( '3', 'Moins de 10€' );
+		
+INSERT INTO `UTILISATION` (  `QUANTITE` , `UNITE` , `ID_RECETTE` , `ID_INGREDIENT` )
+		VALUES
+			( '1', 'Moins de 2€' ),
+			( '2', 'Moins de 5€' ),
+			( '3', 'Moins de 10€' );
+			
+INSERT INTO `CONTENANT` ( `ID_RECETTE`, `ID_ETAPE` )
+		VALUES
+			( '1', 'Moins de 2€' ),
+			( '2', 'Moins de 5€' ),
+			( '3', 'Moins de 10€' );
+			
+INSERT INTO `COMPOSITION` ( `ID_INGREDIENT`, `ID_ETAPE` )
+		VALUES
+			( '1', 'Moins de 2€' ),
+			( '2', 'Moins de 5€' ),
+			( '3', 'Moins de 10€' );
+			
+INSERT INTO `CREATION` ( `ID_RECETTE`, `ID_INTERNAUTE` )
+		VALUES
+			( '1', 'Moins de 2€' ),
+			( '2', 'Moins de 5€' ),
+			( '3', 'Moins de 10€' );
